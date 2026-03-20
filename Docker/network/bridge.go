7088e093ddca8a5159c1206d023432829e27f6a2
22-7-co <x22han7@gmail.com>
2026-03-20 22:45:11 +0800

    22:44

--- Docker/network/bridge.go
+++ Docker/network/bridge.go
@@ -5,6 +5,10 @@ import (
 	"net"
 	"os/exec"
 	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 )
 
 type BridgeNetworkDriver struct {
@@ -15,14 +19,66 @@ func (b *BridgeNetworkDriver) Name() string {
 }
 
 func (b *BridgeNetworkDriver) Create(subnet string, name string) (*NetWork, error) {
-	return nil, nil
+	_, ipRange, _ := net.ParseCIDR(subnet)
+	ip, err := ipAllocator.Allocate(ipRange)
+	if err != nil {
+		return nil, err
+	}
+	ipRange.IP = ip
+	n := &NetWork{
+		Name:      name,
+		IpRange:   ipRange,
+		Driver:    b.Name(),
+		GatewayIp: ip,
+		Subnet:    subnet,
+	}
+	log.Infof("BridgeNetworkDriver creat network subnet: %s, gateway ip: %s", ipRange.String(), ip.String())
+	return n, b.initBridge(n)
 }
 
 func (b *BridgeNetworkDriver) Delete(network NetWork) error {
-	return nil
+	bridgeName := network.Name
+	br, err := netlink.LinkByName(bridgeName)
+	if err != nil {
+		return err
+	}
+	return netlink.LinkDel(br)
 }
 
 func (b *BridgeNetworkDriver) Connect(network *NetWork, endpoint *Endpoint) error {
+	// 获取网络名， linux bridge 的接口名
+	bridgeName := network.Name
+	br, err := netlink.LinkByName(bridgeName)
+	if err != nil {
+		return err
+	}
+
+	// 创建veth 接口的配置
+	la := netlink.NewLinkAttrs()
+	// 由于linux接口名的限制，名字取endpoint id 的前5位
+	la.Name = endpoint.ID[:5]
+	// 通过设置veth接口的master属性，设置这个veth的一端挂载到网络对应的linux bridge上
+	la.MasterIndex = br.Attrs().Index
+
+	//创建Veth对象，通过peerName配置veth另外一端的接口名
+	// 配置Veth另外一端的名字cif-{endpoint id 的前5位}
+	endpoint.Device = netlink.Veth{
+		LinkAttrs: la,
+		PeerName:  "cif-" + endpoint.ID[:5],
+	}
+
+	// 调用netlink的linkadd方法创建出这个veth接口
+	// 因为上面指定了link的masterIndex是网络对应的linux bridge
+	// 所以Veth的一端就已经挂载到了网络对应的linux bridge上了
+	if err = netlink.LinkAdd(&endpoint.Device); err != nil {
+		return fmt.Errorf("add endpoint device err: %w", err)
+	}
+
+	// 调用netlink的linkSetup方法，设置veth启动
+	// 相当于ip link set xxx up 命令
+	if err = netlink.LinkSetUp(&endpoint.Device); err != nil {
+		return fmt.Errorf("add endpoint device setup err: %w", err)
+	}
 	return nil
 }
 
@@ -31,21 +87,103 @@ func (b *BridgeNetworkDriver) Disconnect(network NetWork, endpoint *Endpoint) er
 }
 
 func (b *BridgeNetworkDriver) initBridge(n *NetWork) error {
+	bridgeName := n.Name
+	if err := createBridgeInterface(bridgeName); err != nil {
+		return err
+	}
+	log.Errorf("createBridgeInterface success")
 
+	gatewayIp := *n.IpRange
+	gatewayIp.IP = n.IpRange.IP
+
+	if err := setInterfaceIp(bridgeName, gatewayIp.String()); err != nil {
+		return err
+	}
+	log.Infof("setInterfaceIp success")
+
+	if err := setInterfaceUp(bridgeName); err != nil {
+		return err
+	}
+	log.Infof("setInterfaceUp success")
+
+	if err := setupIpTables(bridgeName, n.IpRange); err != nil {
+		return err
+	}
+	log.Infof("setInterfaceUp success")
+	return nil
 }
 
 func (b *BridgeNetworkDriver) deleteBridge(n *NetWork) error {
-
+	bridgeName := n.Name
+	l, err := netlink.LinkByName(bridgeName)
+	if err != nil {
+		return fmt.Errorf("get link with name %s failed: %w", bridgeName, err)
+	}
+	// delete the link
+	if err := netlink.LinkDel(l); err != nil {
+		return fmt.Errorf("remove bridge interface %s err: %w", bridgeName, err)
+	}
+	return nil
 }
 
-func (b *BridgeNetworkDriver) createBridgeInterface(n string) error {
+func createBridgeInterface(bridgeName string) error {
+	_, err := net.InterfaceByName(bridgeName)
+	if err == nil || !strings.Contains(err.Error(), "no such network interface") {
+		return err
+	}
+
+	la := netlink.NewLinkAttrs()
+	la.Name = bridgeName
+
+	br := &netlink.Bridge{LinkAttrs: la}
+	if err := netlink.LinkAdd(br); err != nil {
+		return fmt.Errorf("bridge creating failed for bridge %s, err: %w", bridgeName, err)
+	}
+	return nil
 }
 
 func setInterfaceUp(interfaceName string) error {
-
+	iface, err := netlink.LinkByName(interfaceName)
+	if err != nil {
+		return fmt.Errorf("error retrieving a link named [ %s ], err: %w", iface.Attrs().Name, err)
+	}
+	if err := netlink.LinkSetUp(iface); err != nil {
+		return fmt.Errorf("error enabling interface for %s, err: %w", interfaceName, err)
+	}
+	log.Infof("ip link set up: %s -- %d", interfaceName, iface.Attrs().Index)
+	return nil
 }
 
-func setInterfaceIp(interfaceName string) error {
+func setInterfaceIp(name string, rawIp string) error {
+	retries := 2
+	var iface netlink.Link
+	var err error
+	for i := 0; i < retries; i++ {
+		iface, err = netlink.LinkByName(name)
+		if err == nil {
+			break
+		}
+		log.Debugf("error retrieving new bridge netlink link [ %s ]... retrying", name)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		return fmt.Errorf("abandoning retrieving the new bridge ink from netlink, run [ip link] to troubleshoot the err: %w", err)
+	}
+	ipNet, err := netlink.ParseIPNet(rawIp)
+	if err != nil {
+		return fmt.Errorf("netlink.ParseIPNet err: %w", err)
+	}
+
+	addr := &netlink.Addr{
+		IPNet:     ipNet,
+		Peer:      ipNet,
+		Label:     "",
+		Flags:     0,
+		Scope:     0,
+		Broadcast: nil,
+	}
+	log.Infof("add addr, iface: %d -- %s, ip: %s", iface.Attrs().Index, iface.Attrs().Name, addr.String())
+	return netlink.AddrAdd(iface, addr)
 }
 
 /*
---
+++ Docker/network/ipam.go
@@ -0,0 +1,168 @@
+package network
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"path"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+const ipamDefaultAllocatorPath = "/var/run/mydocker/network/ipam/subnet.json"
+
+// IPAM 存放IP地址分配信息
+type IPAM struct {
+	// 分配文件存放路径
+	SubnetAllocatorPath string
+	// 网段和位图算法的数组map，key是网络，value是分配的位图数组
+	Subnets *map[string]string
+}
+
+// 初始化一个IPAMd对象
+var ipAllocator = &IPAM{
+	SubnetAllocatorPath: ipamDefaultAllocatorPath,
+}
+
+// 加载网段地址分配信息
+func (ipam *IPAM) load() error {
+	if _, err := os.Stat(ipam.SubnetAllocatorPath); err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	subnetConfigFile, err := os.Open(ipam.SubnetAllocatorPath)
+	defer subnetConfigFile.Close()
+	if err != nil {
+		return err
+	}
+	subnetJson := make([]byte, 2000)
+	n, err := subnetConfigFile.Read(subnetJson)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(subnetJson[:n], ipam.Subnets)
+	if err != nil {
+		return fmt.Errorf("dump allocation info err: %v", err)
+	}
+	log.Infof("load ipam file from: %s", subnetConfigFile.Name())
+	return nil
+}
+
+// 保存网段地址分配信息
+func (ipam *IPAM) dump() error {
+	ipamConfigFileDir, _ := path.Split(ipam.SubnetAllocatorPath)
+	if _, err := os.Stat(ipamConfigFileDir); err != nil {
+		if os.IsNotExist(err) {
+			os.MkdirAll(ipamConfigFileDir, 0755)
+		} else {
+			return err
+		}
+	}
+
+	subnetConfigFile, err := os.OpenFile(ipam.SubnetAllocatorPath,
+		os.O_TRUNC|os.O_WRONLY|os.O_CREATE, 0644)
+	defer subnetConfigFile.Close()
+	if err != nil {
+		return err
+	}
+
+	ipamConfigJson, err := json.Marshal(ipam.Subnets)
+	if err != nil {
+		return err
+	}
+	_, err = subnetConfigFile.Write(ipamConfigJson)
+	if err != nil {
+		return err
+	}
+	log.Infof("dump ipam file from: %s", ipamConfigFileDir)
+	return nil
+}
+
+// 在网段中分配一个可用的IP地址
+func (ipam *IPAM) Allocate(subnet *net.IPNet) (ip net.IP, err error) {
+	// 存储网段中地址分配信息的数组
+	ipam.Subnets = &map[string]string{}
+
+	//从文件中加载已经分配了的网络信息
+	err = ipam.load()
+	if err != nil {
+		return nil, fmt.Errorf("load subnet file err: %v", err)
+	}
+
+	// net.ipnet.nask.size() 返回网段的子网掩码的总长度和网段前面的固定位的长度
+	// 比如 127.0.0.0/8 网段的子网掩码是 255.0.0.0
+	// 返回的是前面255所对应的位数和总位数，即8和24
+	one, size := subnet.Mask.Size()
+
+	// 如果之前没有分配过这个网段，则初始化网段的分配配置
+	if _, exist := (*ipam.Subnets)[subnet.String()]; !exist {
+		// 用0填满这个网段的配置， 1<<uint8(size-one)表示这个网段中有多少个可用的地址
+		// size - one 是子网掩码后面的网络位数，2^（size-one）(即1<<uint8(size-one))表示可用的IP数
+		(*ipam.Subnets)[subnet.String()] = strings.Repeat("0", 1<<uint8(size-one))
+	}
+
+	// 遍历网段的位图数组
+	for c := range (*ipam.Subnets)[subnet.String()] {
+		// 设置当前的序号值为1，即分配这个IP
+		if (*ipam.Subnets)[subnet.String()][c] == '0' {
+			ipalloc := []byte((*ipam.Subnets)[subnet.String()])
+
+			// Go中字符串不能修改，通过转成byte数组，再转成字符串赋能
+			ipalloc[c] = '1'
+			(*ipam.Subnets)[subnet.String()] = string(ipalloc)
+
+			// 这里的IP为初始IP，比如192.168.0.0/16，这里就是192.168.0.0
+			ip = subnet.IP
+
+			// 通过网段的IP与上面的偏移相加计算出分配的IP地址，由于IP地址是uint的一个数组
+			// 需要通过数组中的每一项加所需要的值，比如网段172.16.0.0/12，数组序号是65555
+			// 那么在[172,16,0,0〔方案選單〕]上依次加[uint8(65555 >> 24)、[uint8(65555 >> 16)、[uint8(65555 >> 8)、[uint8(65555 >> 8)
+			// 即[0, 1, 0, 19],那么最后得到的172.17.0.19
+			for t := uint(4); t > 0; t-- {
+				[]byte(ip)[4-t] += uint8(c >> (8 * (t - 1)))
+			}
+
+			// 由于IP是从1开始的，所以加1
+			ip[3] += 1
+			break
+		}
+	}
+	log.Infof("allocate subnet: %s, ip: %s", subnet.String(), ip.String())
+	return ip, ipam.dump()
+}
+
+func (ipam *IPAM) Release(subnet *net.IPNet, ipaddr *net.IP) error {
+	log.Infof("release subnet: %s, ip: %s", subnet.String(), ipaddr.String())
+	ipam.Subnets = &map[string]string{}
+
+	// 从文件中加载网段分配信息
+	err := ipam.load()
+	if err != nil {
+		return fmt.Errorf("load subnet file err: %v", err)
+	}
+
+	// 计算IP地址在网段位图数组中的索引位置
+	index := 0
+	// 将IP地址转换成4个字节的表现形式
+	releaseIp := ipaddr.To4()
+	// 由于IP是从1开始分配的，所以转换成索引应减一
+	releaseIp[3] -= 1
+	// 与分配IP相反，释放IP获得索引的方式是将IP地址的每一位相减后分别左移将对应的数值加到索引上
+	for t := uint(4); t > 0; t -= 1 {
+		index += int(releaseIp[t-1]-subnet.IP[t-1]) << ((4 - t) * 8)
+	}
+	log.Infof("release index: %d", index)
+
+	// 将分配的位图索引中的位置的值为0
+	ipalloc := []byte((*ipam.Subnets)[subnet.String()])
+	ipalloc[index] = '0'
+	(*ipam.Subnets)[subnet.String()] = string(ipalloc)
+
+	// 保存释放IP后的配置信息
+	return ipam.dump()
+}
